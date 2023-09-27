c9e8a9abcf478138b9f28fbce253e4d0737d1ec0
“Kevin-Jimenez-D” <kevinjimenezd97@gmail.com>
2023-09-27 13:15:49 -0500

    feat: :sparkles: Se envia datos con POST y se arreglo para que se vea en el terminal
    Voy a mirar si se aplica en gitignore

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
-/*
-const enviar=async()=>{
-    let config=
+//Sin repetir IDs o sino hay error
+const enviar = async () => {
+    let config =
     {
-        method:"POST",
-        headers:{"Content-Type":"application/json"},
-        body:JSON.stringify({titulo:"HOLA",
-    fecha:"2020",
-    autor:"pachon"
-    })
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            id:2,
+            titulo: "HOLA",
+            fecha: "2020",
+            autor: "pachon"
+        })
     }
-}
-*/
 
-const consultar=async()=>{
-    let res = await ( await fetch ("http://127.0.0.1:5010/libros")).json();
+
+
+
+let res = await(await fetch("http://127.0.0.1:5010/libros", config)).json();
     console.log(res)
 }
 
-consultar();
+enviar()
